007f7036ead5f3d3ecbda8923cf6eb9255b5b2c6
MaximeRenoux <renoux.maxime@ntymail.com>
2023-04-03 15:50:50 +0200

    tp3 episode 1 finit

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetaDataProviderTest.java
@@ -0,0 +1,26 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Categories;
+import org.junit.experimental.categories.Category;
+import static org.junit.Assert.*;
+
+//@Category(IPokemonMetaDataProvider.class)
+public class IPokemonMetaDataProviderTest {
+	//wrong class
+//	@Test
+//	public void indexRangeTest() {
+//		//p.getPokemonMetadata(-1);
+//		PokemonMetadata tooHigh = new PokemonMetadata(151, "carapuce", 10, 20, 30);
+//		PokemonMetadata tooLow = new PokemonMetadata(-1, "carapuce", 10, 20, 30);	
+//		
+//	}
+//	
+//	@Test
+//	public void pokemonMetaData(IPokemonMetadataProvider p, PokemonMetadata m, int index) throws PokedexException {
+//		
+//		assertEquals(m, p.getPokemonMetadata(index));
+//		
+//	}
+	
+}
