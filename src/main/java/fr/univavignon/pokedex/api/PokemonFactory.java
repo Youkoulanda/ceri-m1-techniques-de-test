50855e97d8ab667709b52e05daf1cb00f84137fe
MaximeRenoux <renoux.maxime@ntymail.com>
2023-05-02 18:13:19 +0200

    PokemonFactory implemented

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -2,12 +2,32 @@ package fr.univavignon.pokedex.api;
 
 public class PokemonFactory implements IPokemonFactory{
 	
+	PokemonMetadataProvider pokemonMetadataprovider;
 	
+	public PokemonFactory(PokemonMetadataProvider pokemonMetadataprovider){
+		this.pokemonMetadataprovider = pokemonMetadataprovider;
+	}
 
 	@Override
-	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {	
 		
-		return null;
+		Pokemon pokemon;
+		
+		try {
+			String name = pokemonMetadataprovider.getPokemonMetadata(index).getName();
+			int attack = pokemonMetadataprovider.getPokemonMetadata(index).getAttack();
+			int defense = pokemonMetadataprovider.getPokemonMetadata(index).getDefense();
+			int stamina = pokemonMetadataprovider.getPokemonMetadata(index).getStamina();
+			double iv = 0.9;
+			
+			pokemon = new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
+			
+		} catch (PokedexException e) {
+			e.printStackTrace();
+			return null;
+		}	
+		
+		return pokemon;
 	}
 
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -3,6 +3,8 @@ package fr.univavignon.pokedex.api;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
+import java.util.HashMap;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -14,21 +16,24 @@ public class IPokemonFactoryTest {
 	private IPokemonFactory pokemonFactoryMock;
 	private Pokemon pokemon;
 	private int index;
+	String name = "Bulbizarre";
+	int attack = 126;
+	int defense = 126;
+	int stamina = 90;
 	
 	@Before
 	public void setup() {
-		pokemonFactoryMock = mock(IPokemonFactory.class);
+		HashMap<Integer, PokemonMetadata> pokemonMetadata = new HashMap<Integer, PokemonMetadata>();
+		pokemonMetadata.put(0, new PokemonMetadata(0, name, attack, defense, stamina));
+		pokemonMetadata.put(0, new PokemonMetadata(133, "Aquali", 186, 168, 260));
+		PokemonMetadataProvider pokemonMetadataProvider = new PokemonMetadataProvider(pokemonMetadata);
+		pokemonFactoryMock = new PokemonFactory(pokemonMetadataProvider);
 	}
 	
 	@Test
 	public void checkCreatePokemon() {
 		
 		pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
-		
-		Mockito
-		.when(pokemonFactoryMock.createPokemon(index, 10, 20, 2, 3))
-		.thenReturn(pokemon);
-	
 		
 		assertEquals(pokemon.getCp(), 613);
 		assertEquals(pokemon.getHp(), 64);
