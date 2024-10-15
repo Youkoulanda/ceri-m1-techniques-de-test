8dff5ea827403f9bfa74a044fb7891f686f61ffd
uapv2502122 <uapv2502122@e-mc128-20.ad.univ-avignon.fr>
2024-10-15 08:56:21 +0200

    test methodes IpokemonTrainerFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,8 +1,15 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class IPokedexFactoryTest {
 
+  @Test
+    public void createPokedex(){
+        IPokedexFactory iPokedexFactory = Mockito.mock(IPokedexFactory.class);
 
+    }
 
 
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,4 +1,21 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class IPokemonTrainerFactoryTest {
+
+
+
+     @Test
+    public void createTrainer(){
+        IPokemonTrainerFactory iPokemonTrainerFactory = Mockito.mock(IPokemonTrainerFactory.class);
+        PokemonTrainer pokemonTrainer = new  PokemonTrainer("zzz",Team.VALOR, null);
+        Mockito.when(iPokemonTrainerFactory.createTrainer("zzz", Team.VALOR, null)).thenReturn(pokemonTrainer);
+        assertTrue(iPokemonTrainerFactory.createTrainer("zzz", Team.VALOR, null).equals(pokemonTrainer));
+
+
+    }
+
 }
