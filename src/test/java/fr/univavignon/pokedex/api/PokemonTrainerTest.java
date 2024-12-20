d40c07c4391bebf03afb82ba6b6e9f02177be860
Aghiles <loudaaghiles64@gmail.com>
2024-12-20 14:35:52 +0100

    add test 9

--- src/test/java/fr/univavignon/pokedex/api/PokemonTrainerTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonTrainerTest.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 public class PokemonTrainerTest {
-       @Test
-    public void testPokemonTrainerEquality() {
-        // Mocking dependencies
-        IPokedex mockedPokedex1 = Mockito.mock(IPokedex.class);
-        IPokedex mockedPokedex2 = Mockito.mock(IPokedex.class);
+    //    @Test
+    // public void testPokemonTrainerEquality() {
+    //     // Mocking dependencies
+    //     IPokedex mockedPokedex1 = Mockito.mock(IPokedex.class);
+    //     IPokedex mockedPokedex2 = Mockito.mock(IPokedex.class);
 
-        // Creating PokemonTrainer instances
-        PokemonTrainer trainer1 = new PokemonTrainer("Ash", Team.VALOR, mockedPokedex1);
-        PokemonTrainer trainer2 = new PokemonTrainer("Ash", Team.VALOR, mockedPokedex1);
-        PokemonTrainer trainer3 = new PokemonTrainer("Misty", Team.MYSTIC, mockedPokedex2);
+    //     // Creating PokemonTrainer instances
+    //     PokemonTrainer trainer1 = new PokemonTrainer("Ash", Team.VALOR, mockedPokedex1);
+    //     PokemonTrainer trainer2 = new PokemonTrainer("Ash", Team.VALOR, mockedPokedex1);
+    //     PokemonTrainer trainer3 = new PokemonTrainer("Misty", Team.MYSTIC, mockedPokedex2);
 
-        // Verifying equality
-        assertEquals("Trainers with the same attributes should be equal", trainer1, trainer2);
-        assertNotEquals("Trainers with different attributes should not be equal", trainer1, trainer3);
-    }
+    //     // Verifying equality
+    //     assertEquals("Trainers with the same attributes should be equal", trainer1, trainer2);
+    //     assertNotEquals("Trainers with different attributes should not be equal", trainer1, trainer3);
+    // }
 
     // @Test(expected = NullPointerException.class)
     // public void testPokemonTrainerWithNullName() {
